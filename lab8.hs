fbc388d2f6f7d212b9123a8f8548fb60c7db0869
Govind Venkatesh <venkateshgovind1@gmail.com>
2018-11-16 12:57:02 +0000

    lab 8 commands started

--- lab8.hs
+++ lab8.hs
@@ -21,3 +21,13 @@ evalA (Var name) memory = find name memory
 evalA (Number x) _ = x
 evalA (Add x y) memory = (evalA x memory) + (evalA y memory)
 evalA (Mult x y) memory = (evalA x memory) * (evalA y memory)
+
+data Bexp = And Bexp Bexp | Equal Aexp Aexp | Lt Aexp Aexp | Not Bexp
+
+evalB::Bexp -> Memory -> Bool
+evalB (And bexp1 bexp2) mem = (evalB bexp1 mem) && (evalB bexp2 mem)
+evalB (Not bexp) mem = inverse (evalB bexp mem) where inverse = \f -> if f then False else True
+evalB (Equal exp1 exp2) mem = (evalA exp1 mem) == (evalA exp2 mem)
+evalB (Lt exp1 exp2) mem = (evalA exp1 mem) < (evalA exp2 mem)
+
+data Comm = Ass Aexp | Seq Comm Comm | If Bexp Comm Comm | While Bexp Comm
